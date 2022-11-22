32f11ee20ecba4a533bc080d994409fae19f9aab
MiriamBlanco <114562005+MiriamBlanco@users.noreply.github.com>
2022-11-22 10:29:41 +0100

    Update script1.sh

--- Ejercicios/script1.sh
+++ Ejercicios/script1.sh
@@ -1,7 +1,7 @@
 - Crea un script que añada un puerto de escucha en el fichero de configuración de Apache. 
 El puerto se recibirá como parámetro en la llamada y se comprobará que no esté ya presente en el fichero de configuración.
 
-```bash
+```sh
 #!/bin/bash
 
 echo "¿Qué puerto quieres poner?"
